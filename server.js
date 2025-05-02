6479570d76795bfcc9f42cfb0951976957a77eee
Ayush More <129107864+Ayush-More@users.noreply.github.com>
2025-05-02 17:10:49 +0530

    Revert "Socket issue fixed"
    This reverts commit 7d6ffc2f568435c78953800e5ab23d298b296494.

--- server.js
+++ server.js
@@ -1,33 +1,9 @@
-// // Path: /server.js
-
-// const { createServer } = require('http');
-// const { parse } = require('url');
-// const next = require('next');
-// const { initSocket } = require('./src/lib/socket');
-
-// const dev = process.env.NODE_ENV !== 'production';
-// const app = next({ dev });
-// const handle = app.getRequestHandler();
-
-// app.prepare().then(() => {
-//     const server = createServer((req, res) => {
-//         const parsedUrl = parse(req.url, true);
-//         handle(req, res, parsedUrl);
-//     });
-
-//     // Initialize Socket.IO
-//     initSocket(server);
-
-//     const PORT = process.env.PORT || 3000;
-//     server.listen(PORT, () => {
-//         console.log(`> Ready on http://localhost:${PORT}`);
-//     });
-// });
+// Path: /server.js
 
 const { createServer } = require('http');
 const { parse } = require('url');
 const next = require('next');
-const { Server } = require('socket.io');
+const { initSocket } = require('./src/lib/socket');
 
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
@@ -39,23 +15,11 @@ app.prepare().then(() => {
         handle(req, res, parsedUrl);
     });
 
-    const io = new Server(server, {
-        cors: {
-            origin: process.env.NEXT_PUBLIC_APP_URL || "https://careforindiansv2.vercel.app",
-            methods: ["GET", "POST"],
-            credentials: true,
-            transports: ['websocket', 'polling']
-        },
-        // Add this configuration
-        allowEIO3: true,
-        path: '/api/socketio',
-    });
-
-    // Socket.IO logic
-    require('./lib/socket')(io);
+    // Initialize Socket.IO
+    initSocket(server);
 
     const PORT = process.env.PORT || 3000;
     server.listen(PORT, () => {
-        console.log(`> Ready on ${process.env.NEXT_PUBLIC_APP_URL || `http://localhost:${PORT}`}`);
+        console.log(`> Ready on http://localhost:${PORT}`);
     });
 });
