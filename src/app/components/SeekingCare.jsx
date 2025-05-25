6a6b44614a3756e4247aff23f0ffd27daa85b662
Ayush More <129107864+Ayush-More@users.noreply.github.com>
2025-05-26 00:32:29 +0530

    Motivated to login

--- src/app/components/SeekingCare.jsx
+++ src/app/components/SeekingCare.jsx
@@ -55,22 +55,29 @@
 //     </div>
 //   );
 // }
+'use client'
+import { useRouter} from "next/navigation";
 
 export default function SeekingCare() {
+    const router = useRouter();
   return (
     <div className="flex flex-col md:flex-row mt-[83px] md:gap-[35px] gap-0">
       <div className="md:w-[41%]">
-        <div className="text-[35px] md:text-[46px] font-[500] md:leading-[54px]">
+        <div className="text-[20px] md:text-[30px] font-[500] md:leading-[44px]">
           What Type of Care Service Do You Need?
         </div>
         <div className="text-[#475467] text-[14px] mt-[13px]">
           We are dedicated to connecting you with qualified caregivers and service providers 
           across India to meet your specific care needs
         </div>
+        
+        <button className="cursor-pointer border py-[10px] px-[30px] mt-[25px] rounded-full text-[#081253] text-[15px] hover:bg-[#EF5744] hover:text-white hover:border-[#EF5744] transition-all duration-300 font-[500]" onClick={()=>router.push("/login")}>
+          Login to Access Services
+        </button>
       </div>
       <div className="w-full flex flex-col md:flex-row gap-[15px] md:gap-[11px] mt-5 md:mt-0">
         <div className="bg-[#EFFFDF] rounded-2xl md:w-1/2 p-[17px] flex flex-col justify-between gap-[13px]">
-          <h2 className="font-[600] text-[19px]">Care Provider</h2>
+          <h2 className="font-[600] text-[25px]">Care Provider</h2>
           <ul className="text-[#475467] text-[12px] flex flex-col gap-[13px]">
             <li className="flex gap-[6px]">
               <img src="/Icons/tick-circle.svg" className="h-[32px]" alt="" />
@@ -84,13 +91,15 @@ export default function SeekingCare() {
               <img src="/Icons/tick-circle.svg" className="h-[32px]" alt="" />
               Access mental & physical healthcare specialists
             </li>
+            <li className="flex gap-[6px]">
+              <img src="/Icons/tick-circle.svg" className="h-[32px]" alt="" />
+              Connect with experienced childcare professionals
+            </li>
           </ul>
-          <button className="cursor-pointer border py-[8px] rounded-full text-[#081253] text-[14px] hover:bg-[#EF5744] hover:text-white hover:border-[#EF5744] transition-all duration-300">
-            Find Care Provider
-          </button>
+          
         </div>
         <div className="bg-[#E9ECFF] rounded-2xl md:w-1/2 p-[17px] flex flex-col justify-between gap-[13px]">
-          <h2 className="font-[600] text-[19px]">Care Services</h2>
+          <h2 className="font-[600] text-[25px]">Care Services</h2>
           <ul className="text-[#475467] text-[12px] flex flex-col gap-[13px]">
             <li className="flex gap-[6px]">
               <img src="/Icons/tick-circle.svg" className="h-[32px]" alt="" />
@@ -104,10 +113,12 @@ export default function SeekingCare() {
               <img src="/Icons/tick-circle.svg" className="h-[32px]" alt="" />
               Access quality childcare and healthcare services
             </li>
+            <li className="flex gap-[6px]">
+              <img src="/Icons/tick-circle.svg" className="h-[32px]" alt="" />
+              Connect with experienced childcare professionals
+            </li>
           </ul>
-          <button className="cursor-pointer border py-[8px] rounded-full text-[#081253] text-[14px] hover:bg-[#EF5744] hover:text-white hover:border-[#EF5744] transition-all duration-300">
-            Book Services
-          </button>
+          
         </div>
       </div>
     </div>
